e721f36cfbf6934ba8ccb4d29c4a2d1a53800bd8
Jakob Gottlieb Svendsen <jgs@coretech.dk>
2015-08-20 13:20:23 +0200

    Updated Test-JSONOutput.ps1 in Azure Automation.

--- AzureAutomationMain/Test-JSONOutput.ps1
+++ AzureAutomationMain/Test-JSONOutput.ps1
@@ -1,7 +1,16 @@
 workflow Test-JSONOutput
 {
-	
-	$Result = Get-Date
+    [OutputType([string])]
+
+    #Do work
+    $Now = Get-Date
+
+    #Create return object
+	$Result = New-Object -TypeName PSObject
+    Add-Member -InputObject $Result -MemberType NoteProperty  -Name "Status" -Value "Created"
+    Add-Member -InputObject $Result -MemberType NoteProperty  -Name "Message" -Value "The user jgs@coretech.dk was created"
+    Add-Member -InputObject $Result -MemberType NoteProperty  -Name "Timestamp" -Value "$Now"
 	
 	$Result | ConvertTo-Json
 }
+#Test-JSONOutput
